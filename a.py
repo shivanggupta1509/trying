5cc72550aa2ac5d398d4d43758f32ae714bf6a55
shivanggupta1509 <87229803+shivanggupta1509@users.noreply.github.com>
2022-09-15 10:34:37 +0530

    Update a.py

--- a.py
+++ a.py
@@ -6,7 +6,11 @@ import string
 
 import os
 import time
+count = 0
 
+while (count < 3):    
+
+    count = count + 1
 script_version = "1.0"
 script_title   = "Spotify Account Creator By Sh1vxng"
 script_info    = f'''
@@ -78,11 +82,7 @@ class Main:
 				token = req.json()['login_token']
 				self.settitle(script_title)
 				self.clear(script_info)
-count = 0
 
-while (count < 3):    
-
-    count = count + 1
 				print('[>] ACCOUNT CREATED SUCCESSFULLY\n[-] Email: {0}\n[-] Password:{1}\n[-] Username:{2}\n[-] Gender:{3}\n[-] Birth year:{4}\n[-] Birth month:{5}\n[-] Birth day:{6}\n'.format(credentails['email'],credentails['password'],credentails['username'],credentails['gender'],credentails['birth_year'],credentails['birth_month'],credentails['birth_day']))
 				with open('ACCOUNT-SPOTIFY.txt','a') as f:
 				    f.write('[INFO ACCOUNT]\nEmail: {0}\nPassword: {1}\nUsername: {2}\nGender: {3}\nBirth year: {4}\nBirth month: {5}\nBirth day: {6}\nToken: {7}\n___________________\n\n'.format(credentails['email'],credentails['password'],credentails['username'],credentails['gender'],credentails['birth_year'],credentails['birth_month'],credentails['birth_day'],token))
